fe4e2242c5308e9586b38a3c2d375ed6876571ad
shradhareddyy <prods01@pfw.edu>
2025-03-15 15:14:32 -0400

    flaskapp

--- website/__init__.py
+++ website/__init__.py
@@ -1,40 +1,7 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from os import path
-
-# db=SQLAlchemy()
-# DB_NAME ="database.db"
-
-# def create_app():
-#     app=Flask(__name__)
-#     app.config['SECRET_KEY']='shradha2002'
-#     app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
-#     db.init_app(app)
-
-#     from .views import views
-#     from .auth import auth 
-
-#     app.register_blueprint(views,url_prefix='/')
-#     app.register_blueprint(auth,url_prefix='/')
-
-#     from .models import User,Note
-
-#     create_database(app)
-#     return app
-
-# def create_database(app):
-#     if not path.exists('website/'+ DB_NAME):
-#         db.create_all(app=app)
-#         print('Created Database!')
-
-
-
-
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -45,6 +12,15 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    login_manager=LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     from .views import views
     from .auth import auth
 
@@ -54,6 +30,14 @@ def create_app():
     from .models import User, Note
 
     create_database(app)  
+
+    login_manager=LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -2,16 +2,34 @@ from flask import Blueprint,render_template,request,flash,redirect,url_for
 from .models import User
 from werkzeug.security import generate_password_hash,check_password_hash
 from .import db
+from flask_login import login_user,login_required,logout_user,current_user
 
 auth=Blueprint('auth',__name__)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html",boolean=True)
+    if request.method=='POST':
+        email=request.form.get('email')
+        password=request.form.get('password')
+
+
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully ',category='success')
+                login_user(user,remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect!Please try again!',category='error')
+        else:
+            flash('Email doesnot exist',category='error')
+    return render_template("login.html",user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>LOGOUT</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=['GET','POST'])
 
@@ -23,7 +41,11 @@ def signup():
         password=request.form.get('password')
         password2=request.form.get('password2')
 
-        if len(email)<4:
+        user=User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exist.',category='error')
+        elif len(email)<4:
             flash('Email entered should be more than 3 charaters',category = 'error')
         elif len(firstName)<2:
             flash('First name is too short',category='error')
@@ -37,7 +59,8 @@ def signup():
             new_user=User(email=email,firstName=firstName,lastName=lastName,password=generate_password_hash(password,method = 'pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user,remember=True)
             flash('Account created!',category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html",user=current_user)
