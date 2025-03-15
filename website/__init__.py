2d15943b522cd18673ca08ddf2c7b85751bb4c09
shradhareddyy <prods01@pfw.edu>
2025-03-15 00:44:06 -0400

    ..

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,64 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from os import path
+
+# db=SQLAlchemy()
+# DB_NAME ="database.db"
+
+# def create_app():
+#     app=Flask(__name__)
+#     app.config['SECRET_KEY']='shradha2002'
+#     app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+#     db.init_app(app)
+
+#     from .views import views
+#     from .auth import auth 
+
+#     app.register_blueprint(views,url_prefix='/')
+#     app.register_blueprint(auth,url_prefix='/')
+
+#     from .models import User,Note
+
+#     create_database(app)
+#     return app
+
+# def create_database(app):
+#     if not path.exists('website/'+ DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
+
+
+
+
+
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
-    app=Flask(__name__)
-    app.config['SECRET_KEY']='shradha2002'
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'shradha2002'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
-    from .auth import auth 
+    from .auth import auth
 
-    app.register_blueprint(views,url_prefix='/')
-    app.register_blueprint(auth,url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    create_database(app)  
+
     return app
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):  
+        with app.app_context():  
+            db.create_all()  
+            print("Created Database!")
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,43 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from .import db
 
 auth=Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html",boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>LOGOUT</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 
 def signup():
+    if request.method=='POST':
+        email=request.form.get('email')
+        firstName=request.form.get('firstName')
+        lastName=request.form.get('lastName')
+        password=request.form.get('password')
+        password2=request.form.get('password2')
+
+        if len(email)<4:
+            flash('Email entered should be more than 3 charaters',category = 'error')
+        elif len(firstName)<2:
+            flash('First name is too short',category='error')
+        elif len(lastName)<2:
+            flash('Last name is too short',category = 'error')
+        elif password!=password2:
+            flash('passwords do not match',category = 'error')
+        elif len(password)<7:
+            flash('passsword is too short',category = 'error')
+        else:
+            new_user=User(email=email,firstName=firstName,lastName=lastName,password=generate_password_hash(password,method = 'pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!',category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("sign_up.html")
